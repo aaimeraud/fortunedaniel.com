8dba96eeaf0f57eee149545d08e39b96b0b59fcf
Daniel F. <danielfortune.pro@gmail.com>
2026-03-23 17:32:49 +0100

    Update CV link in NewsBanner component

--- app/components/ui/NewsBanner.tsx
+++ app/components/ui/NewsBanner.tsx
@@ -5,7 +5,7 @@ export default function NewsBanner() {
   return (
     <div title="A container that looks like a button it contains a link to Daniel's CV, it's meant to be a news banner to show Daniel's current situation and the button functionnality isn't explicitly said.">
       <a
-        href="/assets/files/cv_daniel_fortune_bordeaux_A4.pdf"
+        href="/assets/files/STAGE_cv_daniel_fortune.pdf"
         target="_blank"
         className="dark:bg-resumewhite dark:text-resumepurple hover:opacity-85 border border-resumepurple/15  dark:border-resumewhite/15 bg-resumepurple transition text-resumewhite px-2 py-1 inline-flex justify-between items-center gap-2 rounded-sm customoutline duration-300">
         <div className=" rounded-full dark:bg-resumepurple bg-resumewhite w-2 h-2">
